33ade820cd6c8c01bbf32248dda8d5adc4ed2551
minori0827 <91402186+minori0827@users.noreply.github.com>
2022-01-06 01:28:15 +0900

    スタートボタン、タイトルロゴ

--- 神隠し探偵小前とクリソベリルの原石/Assets/Scripts/Start/GameStart.cs
+++ 神隠し探偵小前とクリソベリルの原石/Assets/Scripts/Start/GameStart.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
     public void Main()
     {
-        Debug.Log("1");
+        SceneManager.LoadScene ("Main");
     }
 }
